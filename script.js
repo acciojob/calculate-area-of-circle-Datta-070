27562d6f90ef87da43e6d1e184e0c1600a50a78b
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-01-31 15:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
             // Get the radius from the user using the prompt() function
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
-           
+           let result= 3.14159265*radius*radius;
+	 alert(result.toFixed(2));
 }
 calculateArea();
